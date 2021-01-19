3ca6b13e4179af090dfedbec31c6938c68509fb7
deysarkarswarup <deysarkarswarup123@gmail.com>
2021-01-19 05:40:37 +0530

    db and collection creation

--- src/main/java/MongoDB.java
+++ src/main/java/MongoDB.java
@@ -1,5 +1,18 @@
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
+import java.net.UnknownHostException;
+
 public class MongoDB {
-    public static void main(String[] args) {
+    public static MongoClient mongoClient;
+    public static DB database;
+    public static DBCollection test;
+    public static void main(String[] args) throws UnknownHostException {
+        mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+        database = mongoClient.getDB("EmployeeDb");
+        test = database.getCollection("employee");
 
     }
 }
