26f0dd75f97ac2c76d8caaf6d419faefd9585e1b
deysarkarswarup <deysarkarswarup123@gmail.com>
2021-01-19 05:47:40 +0530

    encapsulating...

--- src/main/java/TestObject.java
+++ src/main/java/TestObject.java
@@ -1,5 +1,33 @@
-
-
 public class TestObject {
+    private int employeeID;
+    private int timer =0;
+    private String employeeName ="";
+    public TestObject(){
 
+    }
+
+    public int employeeID(){
+        return employeeID;
+
+    }
+
+    public String employeeName() {
+        return employeeName;
+    }
+
+    public int getTimer() {
+        return timer;
+    }
+
+    public void setMemberID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public void setTimer(int timer) {
+        this.timer = timer;
+    }
+
+    public void employeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
 }
